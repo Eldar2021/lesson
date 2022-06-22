418e7447b3ffcdf084a5e81989736a16f97e7dac
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-06-22 17:53:51 +0600

    added some links

--- sources/sources.dart
+++ sources/sources.dart
@@ -3,3 +3,8 @@ https://dart.dev/guides/language/language-tour#extension-methods
 https://www.geeksforgeeks.org/data-enumeration-in-dart/
 https://www.educative.io/learn
 */
+/*
+https://dart.dev/guides/language/effective-dart
+https://dart.dev/guides/libraries/library-tour
+https://dart.dev/guides/language/specifications/DartLangSpec-v2.10.pdf
+*/
