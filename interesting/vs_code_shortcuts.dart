92e034c5160267507ba36f3cf3d7864ea786f449
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-06-24 10:01:00 +0600

    VSCode Shortcuts,

---
+++ interesting/vs_code_shortcuts.dart
@@ -0,0 +1,35 @@
+/*
+VSCode Shortcuts, Extensions & Settings for Flutter Development
+
+1. Quick Fix
+  MacOS: CMD + .
+  Windows: CTRL + .
+
+2. Show Command Palette
+  MacOS: CMD + Shift + P
+  Windows: CTRL + Shift + P  
+
+3. Search files by name
+  MacOS: CMD + P
+  Windows: CTRL + P
+
+4. Dart: Add Dependencies
+  First, open the command palette and type 
+  "Dart: Add Dependency" 
+  or "Dart: Add Dev Dependency":
+
+5. Flutter & Dart snippets
+  The Dart and Flutter plugins include many snippets that you can use to add 
+  common Flutter widgets quickly.
+
+  You may already be familiar with these:
+
+  stless: Insert a StatelessWidget
+  stful: Insert a StatefulWidget
+  stanim: Insert a StatefulWidget with an AnimationController
+
+6. Keyboard shortcuts list
+  MacOS: CMD + K / CMD+ S
+  Windows: CTRL + K / CTRL + S
+
+*/
