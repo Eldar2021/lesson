b60551d5cf24405bd549880917baffa65bf055e3
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-06-24 10:04:37 +0600

    VSCode Extensions for Flutter Development

---
+++ interesting/vs_code_extebsions_for_flutter_developer.dart
@@ -0,0 +1,19 @@
+/*
+VSCode Extensions for Flutter Development
+Using the right extensions will save you a lot of time and give you 
+superpowers. These are my favourites:
+
+1. Dart Data Class Generator
+When creating model classes, you'll often needs methods such as copyWith(), 
+toString(), toJson(), fromJson(), toMap(), fromMap(), ==, hashCode.
+
+This is tedious and error-prone.
+nstead, it's much better to use the [Dart Data Class Generator] to do the 
+work for you:
+
+
+5. Remove comments
+This is quite useful to remove all the default comments when you create a 
+new Flutter project: [Remove Comments]
+*/
+
