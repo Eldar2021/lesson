abd3b6e4ab5d75bb53049e71922886d726f21c0f
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-02 12:01:07 +0600

    added my vs code extensions

---
+++ interesting/my_vs_code_extension.dart
@@ -0,0 +1,27 @@
+/*
+02/07/22
+
+1) Awesome Flutter Snippets
+
+2) bloc
+
+3) Dart
+
+4) Dart Data Class Generator
+
+5) dart-import
+
+6) Flutter
+
+7) Flutter Intl
+
+8) Material Icon Theme
+
+9) Pubspec Assist
+
+10) Save Typing
+
+11) YAML
+
+12) Code Spell Checker
+*/
