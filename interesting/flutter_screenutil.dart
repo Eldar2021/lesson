7f977b688d3e9615edd73f11e5e6928e37108bfe
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-04 11:19:16 +0600

    added flutter screen util file

---
+++ interesting/flutter_screenutil.dart
@@ -0,0 +1,8 @@
+/*
+flutter_screenutil — Flutter screen adaptation, font adaptation, get screen 
+information
+
+flutter_screenutil
+A flutter plugin for adapting screen and font size.Let your UI display a 
+reasonable layout on different screen sizes!
+*/
