4c3361732f98a9c6063edf9add9d340d725f1abb
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-18 06:14:43 +0600

    deleted some future learnings

--- feature/feature.dart
+++ feature/feature.dart
@@ -1,7 +1,5 @@
 /// difference between composition & inheritance
-/// factory constructors
 /// rethrow keywords
 /// multiple asynchronous events with Streams
 /// async* and yield
 /// common Future and Stream-based APIs
-/// effective dart
