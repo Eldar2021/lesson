12c7d6824634112f89827bdef3af27a881fce66f
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-08-30 13:25:41 +0600

    added flavor link

--- feature/feature.dart
+++ feature/feature.dart
@@ -6,3 +6,6 @@
 /// https://medium.com/flutter-community/unit-testing-a-flutter-graphql-app-e7ab1e9d1a51
 /// https://medium.com/flutter-community/unit-testing-a-flutter-graphql-app-e7ab1e9d1a51
 /// https://www.spurtcommerce.com/blog/general/knowwhat/72-musthave-features-for-an-ecommerce-website
+//  flavor
+/// https://www.chwe.at/2020/10/flutter-flavors/
+/// https://blog.svenadolph.net/flutter-flavors-and-ios-schemes/#set-up-ios-schemes
