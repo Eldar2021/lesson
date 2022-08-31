c1f10edf1e7a38692533befacded15f96d671098
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-08-31 09:57:00 +0600

    added visual studio app center

--- feature/feature.dart
+++ feature/feature.dart
@@ -6,6 +6,12 @@
 /// https://medium.com/flutter-community/unit-testing-a-flutter-graphql-app-e7ab1e9d1a51
 /// https://medium.com/flutter-community/unit-testing-a-flutter-graphql-app-e7ab1e9d1a51
 /// https://www.spurtcommerce.com/blog/general/knowwhat/72-musthave-features-for-an-ecommerce-website
+/// 
 //  flavor
 /// https://www.chwe.at/2020/10/flutter-flavors/
 /// https://blog.svenadolph.net/flutter-flavors-and-ios-schemes/#set-up-ios-schemes
+/// 
+/// visual studio app center
+/// https://visualstudio.microsoft.com/ru/app-center/
+/// https://appcenter.ms/apps (eldiiaralmazbekov-gmail.com)
+/// https://docs.microsoft.com/ru-ru/appcenter/
