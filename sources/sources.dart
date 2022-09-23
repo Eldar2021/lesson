754cee1883d8310fc86638f1f7abe637cdf34c0d
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-09-23 18:19:41 +0600

    refactor some code

--- sources/sources.dart
+++ sources/sources.dart
@@ -4,11 +4,13 @@ https://www.geeksforgeeks.org/data-enumeration-in-dart/
 https://www.educative.io/learn
 https://codewithandrea.com/tutorials/
 */
+
 /*
 https://dart.dev/guides/language/effective-dart
 https://dart.dev/guides/libraries/library-tour
 https://dart.dev/guides/language/specifications/DartLangSpec-v2.10.pdf
 */
+
 /*
 https://dhruvnakum.xyz/
 */
